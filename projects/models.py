04e4185c49408afd2b385db0585fcc962671c98d
mikharkiv <mykhailo.postnikov@ukma.edu.ua>
2021-03-10 19:26:33 +0200

    [BACKEND] Added Client field to Project model; WARN: migration needed

--- projects/models.py
+++ projects/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from clients.models import Client
 
 
 # TODO add comments
@@ -6,6 +7,7 @@ class Project(models.Model):
 	name = models.CharField(max_length=200)
 	description = models.TextField(max_length=2000)
 	team = models.ForeignKey('teams.Team', on_delete=models.RESTRICT, related_name='projects')
+	client = models.ForeignKey('clients.Client', on_delete=models.CASCADE, related_name='projects')
 
 	created_at = models.DateTimeField(auto_now_add=True)
 	updated_at = models.DateTimeField(auto_now=True)
