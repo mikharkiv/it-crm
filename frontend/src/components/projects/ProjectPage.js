42dfbba6deeb779478d29a35e373d3d6f745e946
mikharkiv <mykhailo.postnikov@ukma.edu.ua>
2021-03-23 22:03:50 +0200

    [DEV] [FRONT] Added Project and Task tabs

--- frontend/src/components/projects/ProjectPage.js
+++ frontend/src/components/projects/ProjectPage.js
@@ -1,14 +1,17 @@
 import {observer} from "mobx-react";
 import LoadingIcon from "../LoadingIcon";
-import {Button, Col, message, Row, Space, Typography} from "antd";
+import {Button, Col, message, Row, Space, Tabs, Typography} from "antd";
 import UserBar from "../UserBar";
 import {useHistory, useParams} from "react-router";
 import {useEffect, useMemo} from "react";
 import {useStores} from "../../hooks/use-stores";
 import {ProjectStore} from "../../stores/projects/ProjectStore";
 import TaskComments from "../tasks/TaskComments";
+import TasksPage from "../tasks/TasksPage";
+import DocumentsPage from "../documents/DocumentsPage";
 
 const {Title, Text} = Typography;
+const {TabPane} = Tabs;
 
 const ProjectPage = () => {
 	const {id} = useParams();
@@ -33,29 +36,45 @@ const ProjectPage = () => {
 	projectStore.state === "loading" ? (<LoadingIcon/>) : (
 	<>
 		<Row justify="space-around" style={{marginTop: "20px"}}>
-			<Col span={11} style={{marginBottom: "30px"}}>
+			<Col span={22} style={{marginBottom: "30px"}}>
 				<Title>{projectStore.project.name}</Title>
-				<Space direction="vertical">
-					<Text className="project-card-text">{projectStore.project.description}</Text>
-					<Text><Text strong>Команда: </Text>{projectStore.project.team.name}</Text>
-					<Text strong>Клієнт: </Text>
-					<UserBar size="small" name={projectStore.project.client.name}
-					         avatar={projectStore.project.client.photo}/>
-				</Space>
 			</Col>
-			<Col span={11}>
-				<TaskComments id={id} isProject={true}/>
-			</Col>
-			<Col span={16} style={{marginBottom: "30px"}}>
-				<Button type="primary" disabled={projectStore.state === "loading"} loading={projectStore.state === "loading"}
-				        onClick={() => edit()} style={{marginRight: "15px"}}>
-					Редагувати
-				</Button>
-				<Button type="primary" danger disabled={projectStore.state === "loading"}
-				        loading={projectStore.state === "loading"}
-				        onClick={() => projectStore.remove(removeCallback)}>
-					Видалити
-				</Button>
+			<Col span={22}>
+				<Tabs defaultActiveKey="1" centered>
+					<TabPane key="1" tab="Інформація">
+						<Row justify="space-around">
+							<Col span={11}>
+								<Space direction="vertical">
+									<Text className="project-card-text">{projectStore.project.description}</Text>
+									<Text><Text strong>Команда: </Text>{projectStore.project.team.name}</Text>
+									<Text strong>Клієнт: </Text>
+									<UserBar size="small" name={projectStore.project.client.name}
+									         avatar={projectStore.project.client.photo}/>
+								</Space>
+							</Col>
+							<Col span={11}>
+								<TaskComments id={id} isProject={true}/>
+							</Col>
+						</Row>
+						<Col span={16} style={{marginBottom: "30px"}}>
+							<Button type="primary" disabled={projectStore.state === "loading"} loading={projectStore.state === "loading"}
+							        onClick={() => edit()} style={{marginRight: "15px"}}>
+								Редагувати
+							</Button>
+							<Button type="primary" danger disabled={projectStore.state === "loading"}
+							        loading={projectStore.state === "loading"}
+							        onClick={() => projectStore.remove(removeCallback)}>
+								Видалити
+							</Button>
+						</Col>
+					</TabPane>
+					<TabPane key="2" tab="Задачі">
+						<TasksPage filters={{project: projectStore.project.id}}/>
+					</TabPane>
+					<TabPane key="3" tab="Документ">
+						<DocumentsPage filters={{project: projectStore.project.id}}/>
+					</TabPane>
+				</Tabs>
 			</Col>
 		</Row>
 	</>
--- frontend/src/components/tasks/TaskPage.js
+++ frontend/src/components/tasks/TaskPage.js
@@ -1,4 +1,4 @@
-import {Button, Col, message, Row, Space, Typography} from "antd";
+import {Button, Col, message, Row, Space, Tabs, Typography} from "antd";
 import {observer} from "mobx-react";
 import {useHistory, useParams} from "react-router";
 import {useStores} from "../../hooks/use-stores";
@@ -9,8 +9,10 @@ import LoadingIcon from "../LoadingIcon";
 import UserBar from "../UserBar";
 import TaskUserBar from "./TaskUserBar";
 import TaskComments from "./TaskComments";
+import DocumentsPage from "../documents/DocumentsPage";
 
 const {Text, Title, Link} = Typography;
+const {TabPane} = Tabs;
 
 const TaskPage = () => {
 	const {id} = useParams();
@@ -35,42 +37,56 @@ const TaskPage = () => {
 		taskStore.state === "loading" || Object.keys(taskStore.task).length === 0 ? (<LoadingIcon/>) : (
 			<>
 				<Row justify="space-around" style={{marginTop: "20px"}} className="task-page">
-					<Col span={11} style={{marginBottom: "30px"}}>
+					<Col span={22} style={{marginBottom: "10px"}}>
 						<Title level={3}>{taskStore.task.name}</Title>
-						<Text style={{marginBottom: "20px", display: "block"}}>{taskStore.task.description}</Text>
-						<Space direction="vertical">
-							<Text><Text strong>Дедлайн: </Text>{taskStore.task.deadline}</Text>
-							<Text><Text strong>Створено: </Text>{taskStore.task.created_at}</Text>
-							<Text><Text strong>Бюджет: </Text>{taskStore.task.budget}</Text>
-							<Text><Text strong>Проєкт: </Text>{taskStore.task.project.name}</Text>
-							<Text><Text strong>Автор: </Text><UserBar size="small" avatar={taskStore.task.author.image}
-							                                          name={taskStore.task.author.full_name}/></Text>
-						</Space>
-						<Title level={3} style={{margin: "15px 0"}}>Прикріплені користувачі</Title>
-						<div className="task-page-attached_users">
-							{taskStore.task.hasOwnProperty('attached_persons') &&
-							taskStore.task["attached_persons"].map((e) => (
-								<TaskUserBar key={e.person.id} avatar={e.person.image} name={e.person.full_name} checked={e.is_completed}
-								             canEdit={
-								             	rootStore.me && rootStore.me.hasOwnProperty('id') && e.person.id === rootStore.me.id
-								             }
-								             onChange={(e) => taskStore.setCompleted(e.target.checked)} />
-							))}
-						</div>
 					</Col>
-					<Col span={11}>
-						<TaskComments id={id} isProject={false}/>
-					</Col>
-					<Col span={16} style={{marginBottom: "30px"}}>
-						<Button type="primary" disabled={taskStore.state === "loading"} loading={taskStore.state === "loading"}
-						        onClick={() => edit()} style={{marginRight: "15px"}}>
-							Редагувати
-						</Button>
-						<Button type="primary" danger disabled={taskStore.state === "loading"}
-						        loading={taskStore.state === "loading"}
-						        onClick={() => taskStore.remove(removeCallback)}>
-							Видалити
-						</Button>
+					<Col span={22}>
+						<Tabs defaultActiveKey="1" centered>
+							<TabPane key="1" tab="Інформація">
+								<Row justify="space-between">
+
+									<Col span={11} style={{marginBottom: "30px"}}>
+										<Text style={{marginBottom: "20px", display: "block"}}>{taskStore.task.description}</Text>
+										<Space direction="vertical">
+											<Text><Text strong>Дедлайн: </Text>{taskStore.task.deadline}</Text>
+											<Text><Text strong>Створено: </Text>{taskStore.task.created_at}</Text>
+											<Text><Text strong>Бюджет: </Text>{taskStore.task.budget}</Text>
+											<Text><Text strong>Проєкт: </Text>{taskStore.task.project.name}</Text>
+											<Text><Text strong>Автор: </Text><UserBar size="small" avatar={taskStore.task.author.image}
+											                                          name={taskStore.task.author.full_name}/></Text>
+										</Space>
+										<Title level={3} style={{margin: "15px 0"}}>Прикріплені користувачі</Title>
+										<div className="task-page-attached_users">
+											{taskStore.task.hasOwnProperty('attached_persons') &&
+											taskStore.task["attached_persons"].map((e) => (
+												<TaskUserBar key={e.person.id} avatar={e.person.image} name={e.person.full_name} checked={e.is_completed}
+												             canEdit={
+													             rootStore.me && rootStore.me.hasOwnProperty('id') && e.person.id === rootStore.me.id
+												             }
+												             onChange={(e) => taskStore.setCompleted(e.target.checked)} />
+											))}
+										</div>
+									</Col>
+									<Col span={11}>
+										<TaskComments id={id} isProject={false}/>
+									</Col>
+								</Row>
+								<Col span={16} style={{marginBottom: "30px"}}>
+									<Button type="primary" disabled={taskStore.state === "loading"} loading={taskStore.state === "loading"}
+									        onClick={() => edit()} style={{marginRight: "15px"}}>
+										Редагувати
+									</Button>
+									<Button type="primary" danger disabled={taskStore.state === "loading"}
+									        loading={taskStore.state === "loading"}
+									        onClick={() => taskStore.remove(removeCallback)}>
+										Видалити
+									</Button>
+								</Col>
+							</TabPane>
+							<TabPane key="2" tab="Документи">
+								<DocumentsPage filters={{task: taskStore.task.id}} />
+							</TabPane>
+						</Tabs>
 					</Col>
 				</Row>
 			</>
